5171fb34852eb797e7e50735a9f6cec0c0d37f8d
fangxi <xyzfang@126.com>
2025-07-29 22:02:29 +0800

    再次测试提交

--- main.go
+++ main.go
@@ -3,7 +3,5 @@ import (
 	"fmt")
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("This is a simple Go program.")
-	fmt.Println("It prints messages to the console.")
+	fmt.Println("再次测试提交") 
 }	
