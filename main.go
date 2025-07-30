69dd5cc439d4817306509d726989b96904c7fbfa
fangxi <xyzfang@126.com>
2025-07-30 22:55:05 +0800

    完成任务3

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	// fmt.Println("完成任务1：只出现一次的数字") 
-	// arr :=[9]int{1,2,3,4,5,1,3,4,5}
-	// result := task.Task1(arr)
-	// fmt.Println("只出现一次的数字是:", result)
+	fmt.Println("完成任务1：只出现一次的数字") 
+	arr :=[9]int{1,2,3,4,5,1,3,4,5}
+	result := task.Task1(arr)
+	fmt.Println("只出现一次的数字是:", result)
 
 	fmt.Println("完成任务2：括号匹配")
 	s := "((){}[]"
